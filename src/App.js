f20bbd8773ec9becfe06382305c9ba664565680f
Jibanta <63874888+jibanta66@users.noreply.github.com>
2024-02-14 23:17:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import Sidebar from "./components/sidebar";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Cats from "./pages/teacher";
+import Dogs from "./pages/student" ;
+import ErrorPage from "./pages/404";
+import GoogleSignUp from "./fierbase/auth";
+import Login from "./pages/loginform";
+import Loginteacher from "./pages/teacherlogin";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ flex: 1 }}>
+          <div style={{ maxWidth: "lg", margin: "auto" }}>
+            <Routes>
+              <Route
+                exact
+                path={"React-Sidebar-example/"}
+                element={<Home />}
+              />
+              <Route
+                path={"React-Sidebar-example/teacher"}
+                element={<Cats />}
+              />
+              <Route
+                path={"React-Sidebar-example/student"}
+                element={<Dogs />}
+              />
+
+
+<Route
+                path={"React-Sidebar-example/GoogleSignUp"}
+                element={<GoogleSignUp />}
+              />
+              <Route
+                path={"React-Sidebar-example/Login"}
+                element={<Login />}
+              />
+               <Route
+                path={"React-Sidebar-example/Loginteacher"}
+                element={<Loginteacher/>}
+              />
+             
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
